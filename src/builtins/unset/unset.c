b1c064fa3649ade261080baa30ad6d2d291a20cf
Kelvin Faustino <kfaustin@student.42porto.com>
2023-05-16 10:49:40 +0100

    unset env refresh

--- src/builtins/unset/unset.c
+++ src/builtins/unset/unset.c
@@ -6,19 +6,17 @@
 /*   By: kfaustin <kfaustin@student.42porto.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 11:25:43 by kfaustin          #+#    #+#             */
-/*   Updated: 2023/05/03 15:03:04 by kfaustin         ###   ########.fr       */
+/*   Updated: 2023/05/16 10:47:01 by kfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 // Returns true if the var to unset was located and freed in the key of the first node
-static bool	check_unset_first_node(t_env *env, char *str, int len_str)
+static bool	check_unset_first_node(t_env *env, char *str)
 {
 	t_env	*tmp;
-	int		len_key;
 
-	len_key = (int)ft_strlen(env->key);
-	if ((len_str == len_key) && !ft_strncmp(str, env->key, len_str))
+	if (abs_string_cmp(str, env->key))
 	{
 		free (env->key);
 		free (env->value);
@@ -31,17 +29,15 @@ static bool	check_unset_first_node(t_env *env, char *str, int len_str)
 }
 
 // Returns true if the var to unset was located and freed until the end os the env.
-static bool	check_unset_till_end(t_env *env, char *str, int len_str)
+static bool	check_unset_till_end(t_env *env, char *str)
 {
 	t_env	*header;
 	t_env	*tmp;
-	int		len_key;
 
 	header = env;
 	while (env)
 	{
-		len_key = (int)ft_strlen(env->key);
-		if ((len_str == len_key) && !ft_strncmp(str, env->key, len_str))
+		if (abs_string_cmp(str, env->key))
 		{
 			free (env->key);
 			free (env->value);
@@ -57,18 +53,22 @@ static bool	check_unset_till_end(t_env *env, char *str, int len_str)
 	return (false);
 }
 
-// Be sure that env is not null
 void	builtin_unset(t_msh *data)
 {
 	char	*str;
-	int		len_str;
 
 	str = (char *)data->lst_cmd->argv[1];
-	len_str = (int)ft_strlen(str);
-	if (check_unset_first_node(data->ppt->list, str, len_str))
+	if (check_unset_first_node(data->ppt->list, str))
+	{
+		free_t_env(data->ppt->list);
+		init_env_table(data);
 		return ;
-	if (check_unset_till_end(data->ppt->list, str, len_str))
+	}
+
+	if (check_unset_till_end(data->ppt->list, str))
+	{
+		free_t_env(data->ppt->list);
+		init_env_table(data);
 		return ;
-	// Print to tests only, TO DELETE!
-	ft_putstr_fd("key not found in env list, nothing to unset\n", 2);
+	}
 }
